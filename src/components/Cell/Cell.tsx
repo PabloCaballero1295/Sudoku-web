cc0d1e6abd7e7a8d2f75b3c3a5ee5f2b0e0b75ca
Pablo Sebastián Caballero <pablocaballero1295@gmail.com>
2024-08-20 18:07:28 +0100

    Modified styles

--- src/components/Cell/Cell.tsx
+++ src/components/Cell/Cell.tsx
@@ -24,6 +24,7 @@ export const Cell = ({
 }: CellProps) => {
   //const sudoku = useAppSelector((state) => state.sudoku)
   const sudokuTools = useAppSelector((state) => state.sudokuTools)
+  const sudoku = useAppSelector((state) => state.sudoku)
   const activeCell = sudokuTools.activeCell
 
   const dispatch = useAppDispatch()
@@ -51,6 +52,13 @@ export const Cell = ({
   const getCellStyle = () => {
     let cellStyle = "cell"
 
+    if (row == 8) {
+      cellStyle += " cell-last-row"
+    }
+    if (col == 8) {
+      cellStyle += " cell-last-col"
+    }
+
     if (borderBottom) {
       cellStyle += " bottom-border"
     }
@@ -62,15 +70,20 @@ export const Cell = ({
     if (readOnly) {
       cellStyle += " read-only"
     }
-
     if (activeCell.row == row && activeCell.col == col) {
       cellStyle += " active-cell"
-    } else if (activeCell.row == row || activeCell.col == col) {
+    } else if ((activeCell.row == row || activeCell.col == col) && !readOnly) {
       cellStyle += " active-cell-row-col"
     } else if (
-      checkNextActiveCellBox(activeCell.row, activeCell.col, row, col)
+      checkNextActiveCellBox(activeCell.row, activeCell.col, row, col) &&
+      !readOnly
     ) {
       cellStyle += " active-cell-row-col"
+    } else if (
+      value == sudoku.board[activeCell.row][activeCell.col].value &&
+      value != 0
+    ) {
+      cellStyle += " active-cell-same-number"
     }
 
     if (cellValue != 0 && cellValue != solutionValue) {
@@ -80,26 +93,36 @@ export const Cell = ({
     return cellStyle
   }
 
+  const getInnerBoxStyle = () => {
+    let cellStyle = ""
+    if (activeCell.row == row && activeCell.col == col) {
+      cellStyle += " inner-box"
+    }
+    return cellStyle
+  }
+
   return (
     <>
       <div className={getCellStyle()} onClick={handleClick}>
-        {notes.length == 0 ? (
-          cellValue === 0 ? (
-            ""
+        <div className={getInnerBoxStyle()}>
+          {notes.length == 0 ? (
+            cellValue === 0 ? (
+              ""
+            ) : (
+              cellValue
+            )
           ) : (
-            cellValue
-          )
-        ) : (
-          <div className="notes">
-            {Array(9)
-              .fill("")
-              .map((_, index) => (
-                <div className="notes-number" key={index}>
-                  {notes.includes(index + 1) ? index + 1 : " "}
-                </div>
-              ))}
-          </div>
-        )}
+            <div className="notes">
+              {Array(9)
+                .fill("")
+                .map((_, index) => (
+                  <div className="notes-number" key={index}>
+                    {notes.includes(index + 1) ? index + 1 : " "}
+                  </div>
+                ))}
+            </div>
+          )}
+        </div>
       </div>
     </>
   )
